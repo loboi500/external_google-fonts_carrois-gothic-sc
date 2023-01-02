e1ae3dc31decea6841e36b22fcd12cf7ab3d6d84
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:08:25 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,20 +1,3 @@
-# Copyright (C) 2014 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
-
-PRODUCT_PACKAGES := \
-    CarroisGothicSC-Regular.ttf
+version https://git-lfs.github.com/spec/v1
+oid sha256:83eafe39d99424e73d982d6fc490bd34ee9ad37bdd5e2b33900bfe2e4fe3309d
+size 829
